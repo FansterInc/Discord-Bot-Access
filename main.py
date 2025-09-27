d60f74d9ba3ce084c4e2f2fb0942b2c0d1f43194
fanster <ifansterinc@gmail.com>
2025-09-27 13:19:28 +0700

    created bot loggic

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import asyncio
+import logging
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+# Загружаем переменные из файла .env
+load_dotenv()
+
+token = os.getenv("TOKEN")
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token)
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="Пройти на сервер", url="https://discord.gg/bu5uwrXMtm")
+    )
+    await message.answer(
+        'Чтобы пройти на сервер нажи на кнопку',
+        reply_markup=builder.as_markup(),
+    )
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
